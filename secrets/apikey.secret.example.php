5e3998d375c02f3af15cbf085969a32537b1cb3e
David Roesel <roesel@gmail.com>
2014-09-14 01:26:06 +0200

    Gitignore change
    corrected fail

---
+++ secrets/apikey.secret.example.php
@@ -0,0 +1,7 @@
+<?php
+
+/* -- Defining constants ---------------------------------------------------- */
+// Fill the following, then rename the file to "apikey.secret.php"
+const API_KEY = "apikeyapikeyapikeyapikey"; // LoL Signature generator secret
+
+?>
---
+++ secrets/db.secret.example.php
@@ -0,0 +1,12 @@
+<?php
+
+/* -- Defining databze access ---------------------------------------------------- */
+$mysql_credentials = array(
+                        'driver'   => 'mysql',
+                        'host'     => 'localhost',
+                        'username' => 'root',
+                        'password' => '',
+                        'database' => 'lolcompare',
+                        'charset'  => 'utf8',
+                    );
+?>
