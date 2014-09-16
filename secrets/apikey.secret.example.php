24ccae8a38b93dce09ade11c4393b63d53b42810
David Roesel <roesel@gmail.com>
2014-09-16 22:08:38 +0200

    Moving wait time to secret

--- secrets/apikey.secret.example.php
+++ secrets/apikey.secret.example.php
@@ -3,5 +3,6 @@
 /* -- Defining constants ---------------------------------------------------- */
 // Fill the following, then rename the file to "apikey.secret.php"
 const API_KEY = "apikeyapikeyapikeyapikey"; // LoL Signature generator secret
-
+const MAIN_CACHE_TIME = 30;  // time in seconds, 30 for debug, 6*60*60 for live?
+const API_REQUEST_WAIT = 100000;    // time in microseconds to wait for call
 ?>
