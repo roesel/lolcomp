d95a9c4c7c11fcad00615aa528f6c996ff5cecf5
David Roesel <roesel@gmail.com>
2014-09-17 18:40:35 +0200

    Added Google Analytics script

--- index.php
+++ index.php
@@ -23,6 +23,16 @@ if (isset($_GET["orderby"]) && isset($_GET["way"]))
 	<title>LoLScores - Compare your scores with friends!</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-41604637-2', 'auto');
+  ga('send', 'pageview');
+
+</script>
 <body style="background:url(img/background.jpg);color:white;">
 	<div id="wrapper" style="width:1700px;text-align:left;margin-left:auto;margin-right:auto;">
 		<div id="form" style="float:left;width:300px;margin-left:150px;">
